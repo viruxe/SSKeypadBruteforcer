b7cf8d31a0367f7fbbfb81b6dec63fcd577713ad
Flávio Pereira <viruxe.the.pwner@gmail.com>
2014-02-20 01:26:32 +0000

    Source Import
    Self explainatory.

---
+++ frmMain.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lstCodes = New System.Windows.Forms.ListBox()
+        Me.txtInterval = New System.Windows.Forms.TextBox()
+        Me.tmrScanner = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblCodeCount = New System.Windows.Forms.Label()
+        Me.lstDebug = New System.Windows.Forms.ListBox()
+        Me.chkDebugSound = New System.Windows.Forms.CheckBox()
+        Me.chkScanWithUsedCodes = New System.Windows.Forms.CheckBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.btnClearLog = New System.Windows.Forms.Button()
+        Me.tmrTimePassed = New System.Windows.Forms.Timer(Me.components)
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.chkReverseScan = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.chkDebugActive = New System.Windows.Forms.CheckBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtCodeList = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lstCodes
+        '
+        Me.lstCodes.FormattingEnabled = True
+        Me.lstCodes.Location = New System.Drawing.Point(12, 113)
+        Me.lstCodes.Name = "lstCodes"
+        Me.lstCodes.Size = New System.Drawing.Size(81, 381)
+        Me.lstCodes.TabIndex = 1
+        '
+        'txtInterval
+        '
+        Me.txtInterval.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtInterval.Location = New System.Drawing.Point(79, 22)
+        Me.txtInterval.Name = "txtInterval"
+        Me.txtInterval.Size = New System.Drawing.Size(44, 20)
+        Me.txtInterval.TabIndex = 2
+        Me.txtInterval.Text = "100"
+        Me.txtInterval.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ToolTip1.SetToolTip(Me.txtInterval, "Interval in miliseconds to generate and send a new code.")
+        '
+        'tmrScanner
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Interval (ms):"
+        '
+        'lblCodeCount
+        '
+        Me.lblCodeCount.AutoSize = True
+        Me.lblCodeCount.Location = New System.Drawing.Point(12, 497)
+        Me.lblCodeCount.Name = "lblCodeCount"
+        Me.lblCodeCount.Size = New System.Drawing.Size(46, 13)
+        Me.lblCodeCount.TabIndex = 4
+        Me.lblCodeCount.Text = "0 Codes"
+        '
+        'lstDebug
+        '
+        Me.lstDebug.FormattingEnabled = True
+        Me.lstDebug.Location = New System.Drawing.Point(99, 113)
+        Me.lstDebug.Name = "lstDebug"
+        Me.lstDebug.Size = New System.Drawing.Size(224, 381)
+        Me.lstDebug.TabIndex = 5
+        '
+        'chkDebugSound
+        '
+        Me.chkDebugSound.AutoSize = True
+        Me.chkDebugSound.Location = New System.Drawing.Point(68, 14)
+        Me.chkDebugSound.Name = "chkDebugSound"
+        Me.chkDebugSound.Size = New System.Drawing.Size(57, 17)
+        Me.chkDebugSound.TabIndex = 6
+        Me.chkDebugSound.Text = "Sound"
+        Me.chkDebugSound.UseVisualStyleBackColor = True
+        '
+        'chkScanWithUsedCodes
+        '
+        Me.chkScanWithUsedCodes.AutoSize = True
+        Me.chkScanWithUsedCodes.Location = New System.Drawing.Point(185, 12)
+        Me.chkScanWithUsedCodes.Name = "chkScanWithUsedCodes"
+        Me.chkScanWithUsedCodes.Size = New System.Drawing.Size(120, 17)
+        Me.chkScanWithUsedCodes.TabIndex = 7
+        Me.chkScanWithUsedCodes.Text = "Scan with Code List"
+        Me.ToolTip1.SetToolTip(Me.chkScanWithUsedCodes, "Start cracking with the already used codes list in backwards.")
+        Me.chkScanWithUsedCodes.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.DefaultExt = "codes"
+        Me.OpenFileDialog1.Filter = "Code files|*.codes|All files|*.*"
+        Me.OpenFileDialog1.Title = "Select a code list to open!"
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.DefaultExt = "codes"
+        Me.SaveFileDialog1.Filter = "Code files|*.codes|All files|*.*"
+        '
+        'btnClearLog
+        '
+        Me.btnClearLog.Location = New System.Drawing.Point(236, 500)
+        Me.btnClearLog.Name = "btnClearLog"
+        Me.btnClearLog.Size = New System.Drawing.Size(87, 30)
+        Me.btnClearLog.TabIndex = 11
+        Me.btnClearLog.Text = "Clear Log"
+        Me.btnClearLog.UseVisualStyleBackColor = True
+        '
+        'tmrTimePassed
+        '
+        Me.tmrTimePassed.Interval = 1000
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(232, 4)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(46, 23)
+        Me.btnLoad.TabIndex = 21
+        Me.btnLoad.Text = "Load"
+        Me.ToolTip1.SetToolTip(Me.btnLoad, "Load or Save a code list file")
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 13)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Codes Tried:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(96, 97)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Debug Window:"
+        '
+        'chkReverseScan
+        '
+        Me.chkReverseScan.AutoSize = True
+        Me.chkReverseScan.Enabled = False
+        Me.chkReverseScan.Location = New System.Drawing.Point(185, 35)
+        Me.chkReverseScan.Name = "chkReverseScan"
+        Me.chkReverseScan.Size = New System.Drawing.Size(94, 17)
+        Me.chkReverseScan.TabIndex = 17
+        Me.chkReverseScan.Text = "Reverse Scan"
+        Me.chkReverseScan.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.chkDebugActive)
+        Me.GroupBox1.Controls.Add(Me.chkDebugSound)
+        Me.GroupBox1.Location = New System.Drawing.Point(99, 497)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(131, 37)
+        Me.GroupBox1.TabIndex = 18
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Debug:"
+        '
+        'chkDebugActive
+        '
+        Me.chkDebugActive.AutoSize = True
+        Me.chkDebugActive.Location = New System.Drawing.Point(6, 14)
+        Me.chkDebugActive.Name = "chkDebugActive"
+        Me.chkDebugActive.Size = New System.Drawing.Size(56, 17)
+        Me.chkDebugActive.TabIndex = 7
+        Me.chkDebugActive.Text = "Active"
+        Me.chkDebugActive.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Enabled = False
+        Me.btnSave.Location = New System.Drawing.Point(278, 4)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(45, 23)
+        Me.btnSave.TabIndex = 22
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 13)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Code List Filename:"
+        '
+        'txtCodeList
+        '
+        Me.txtCodeList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtCodeList.Location = New System.Drawing.Point(114, 7)
+        Me.txtCodeList.Name = "txtCodeList"
+        Me.txtCodeList.Size = New System.Drawing.Size(112, 20)
+        Me.txtCodeList.TabIndex = 19
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.chkScanWithUsedCodes)
+        Me.GroupBox2.Controls.Add(Me.chkReverseScan)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.txtInterval)
+        Me.GroupBox2.Location = New System.Drawing.Point(9, 33)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(314, 55)
+        Me.GroupBox2.TabIndex = 23
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Scan Options:"
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Location = New System.Drawing.Point(9, 517)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(30, 13)
+        Me.lblTime.TabIndex = 24
+        Me.lblTime.Text = "Time"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(332, 539)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtCodeList)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnClearLog)
+        Me.Controls.Add(Me.lstDebug)
+        Me.Controls.Add(Me.lblCodeCount)
+        Me.Controls.Add(Me.lstCodes)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SS Keypad Cracker"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lstCodes As System.Windows.Forms.ListBox
+    Friend WithEvents txtInterval As System.Windows.Forms.TextBox
+    Public WithEvents tmrScanner As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblCodeCount As System.Windows.Forms.Label
+    Friend WithEvents lstDebug As System.Windows.Forms.ListBox
+    Friend WithEvents chkDebugSound As System.Windows.Forms.CheckBox
+    Friend WithEvents chkScanWithUsedCodes As System.Windows.Forms.CheckBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents btnClearLog As System.Windows.Forms.Button
+    Friend WithEvents tmrTimePassed As System.Windows.Forms.Timer
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents chkReverseScan As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents chkDebugActive As System.Windows.Forms.CheckBox
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnLoad As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtCodeList As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblTime As System.Windows.Forms.Label
+
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,260 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class frmMain
+
+    'Private Declare Function GetAsyncKeyState Lib "User32" (ByVal vKey As Integer) As Short
+
+    'Function GetKey(ByVal Key As Integer) As Boolean
+    '    If GetAsyncKeyState(Key) = -32767 Then
+    '        Return True
+    '    End If
+    '    Return False
+    'End Function
+
+    Public Const MOD_ALT As Integer = &H1 'Alt key
+    Public Const VK_SNAPSHOT As Integer = &H2C 'PrintScreen key
+    Public Const WM_HOTKEY As Integer = &H312
+
+    'P/Invoke API
+    Public Declare Function RegisterHotKey Lib "user32" (ByVal hwnd As IntPtr, ByVal id As Integer, ByVal fsModifiers As Integer, ByVal vk As Integer) As Integer
+    Public Declare Function UnregisterHotKey Lib "user32" (ByVal hwnd As IntPtr, ByVal id As Integer) As Integer
+
+    Private curCodeListPath As String 'Store the file path
+    Private doKeys As Boolean = True 'Send keys or not (debug purposes)
+    Private startDate As Date
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        If m.Msg = WM_HOTKEY Then
+
+            'Start
+            My.Computer.Audio.Play("c:\Windows\Media\Speech On.wav")
+            If Not tmrScanner.Enabled Then
+                If Not chkScanWithUsedCodes.Checked And lstCodes.Items.Count > 0 Then
+                    If MessageBox.Show("Do you want to clear the codes?", "Clear Codes?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then lstCodes.Items.Clear()
+                End If
+
+                tmrScanner.Interval = Val(txtInterval.Text)
+                tmrScanner.Start()
+                startDate = DateTime.Now
+                lblTime.Text = GetTimeDuration()
+                tmrTimePassed.Start()
+                txtInterval.Enabled = False
+                txtCodeList.Enabled = False
+                btnLoad.Enabled = False
+                chkScanWithUsedCodes.Enabled = False
+                chkReverseScan.Enabled = False
+                Log("> Process started")
+                If lstCodes.Items.Count = 0 And chkScanWithUsedCodes.Checked = True Then Log("> No codes to scan you noob")
+
+            Else 'Stop
+                My.Computer.Audio.Play("c:\Windows\Media\Speech Off.wav")
+                tmrScanner.Stop()
+                txtInterval.Enabled = True
+                txtCodeList.Enabled = True
+                btnLoad.Enabled = True
+                chkScanWithUsedCodes.Enabled = True
+                chkReverseScan.Enabled = True
+
+                tmrTimePassed.Stop()
+                Log("> Process ended (Time taken: " & lblTime.Text & ")")
+            End If
+        End If
+        MyBase.WndProc(m)
+    End Sub
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call RegisterHotKey(Me.Handle, 9, MOD_ALT, VK_SNAPSHOT)
+        lblTime.Text = String.Empty
+    End Sub
+
+    Private Sub frmMain_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        Call UnregisterHotKey(Me.Handle, 9)  'Remember to unregister the hotkey
+    End Sub
+
+    Private Function GenerateNewCode() As String
+        Dim rand As New Random
+        Dim code As String
+        Dim tries As Integer
+
+        Do
+            code = rand.Next(0, 9999)
+
+            If code < 10 Then
+                Log("Found a code under 10! (" & code & ")", debugLog:=True)
+                code = code.Insert(0, "0")
+                If chkDebugSound.Checked Then My.Computer.Audio.Play("c:\Windows\Media\recycle.wav")
+            End If
+
+            If code < 100 Then
+                Log("Found a code under 100! (" & code & ")", debugLog:=True)
+                code = code.Insert(0, "0")
+                If chkDebugSound.Checked Then My.Computer.Audio.Play("c:\Windows\Media\recycle.wav")
+            End If
+
+            If code < 1000 Then
+                Log("Found a code under 1000! (" & code & ")", debugLog:=True)
+                code = code.Insert(0, "0")
+                If chkDebugSound.Checked Then My.Computer.Audio.Play("c:\Windows\Media\chimes.wav")
+            End If
+
+            tries = +1
+            If tries > 1 Then Log(code)
+        Loop While IsCodeDuplicated(code) = True
+
+        Return code
+    End Function
+
+    ''' <summary>
+    ''' Check if the provided code is listed on the codelist
+    ''' </summary>
+    ''' <param name="code"></param>
+    ''' <returns>Boolean</returns>
+    ''' <remarks></remarks>
+    Private Function IsCodeDuplicated(ByVal code)
+        For Each usedCode As String In lstCodes.Items
+            If usedCode = code Then
+                Log("Duplicated code! (" & code & ")", True)
+                Return True
+            End If
+        Next
+
+        Return False
+    End Function
+
+    Private Sub tmrScanner_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrScanner.Tick
+        If chkScanWithUsedCodes.Checked Then 'Loop trough listbox
+            If lstCodes.Items.Count > 0 Then
+                Static curIndex As Integer
+
+                If curIndex = 0 Then
+                    curIndex = lstCodes.Items.Count - 1 'Reset counter
+                ElseIf chkReverseScan.Checked And curIndex = lstCodes.Items.Count - 1 Then
+                    curIndex = 0
+                End If
+
+                Try
+                    If doKeys Then SendKeys.Send(lstCodes.Items.Item(curIndex) & "~")
+                    lstCodes.SelectedIndex = curIndex
+                Catch ex As Exception
+                    Log("ERROR: " & ex.Message)
+                End Try
+
+                If chkReverseScan.Checked Then curIndex += 1 Else curIndex -= 1
+
+                lblCodeCount.Text = lstCodes.Items.Count & " Codes"
+            End If
+        Else
+            Dim code As String = GenerateNewCode()
+
+            'Add to list
+            lstCodes.Items.Add(code)
+            lstCodes.SelectedIndex = lstCodes.Items.Count - 1
+            lblCodeCount.Text = lstCodes.Items.Count & " Codes"
+
+            'Send keys
+            If doKeys Then
+                SendKeys.Send(code & "~")
+            Else
+                Log(code & "~", debugLog:=True)
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Log(ByVal message As String, Optional ByVal debugLog As Boolean = False)
+        If debugLog = True And Not chkDebugActive.Checked Then Exit Sub
+
+        lstDebug.Items.Add(message)
+        lstDebug.SelectedIndex = lstDebug.Items.Count - 1
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        lstCodes.Items.Clear()
+    End Sub
+
+    Private Sub btnClearLog_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClearLog.Click
+        lstDebug.Items.Clear()
+    End Sub
+
+    Private Sub btnLoad_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLoad.Click
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            curCodeListPath = OpenFileDialog1.FileName
+
+            txtCodeList.Text = OpenFileDialog1.SafeFileName
+            txtCodeList.SelectionStart = txtCodeList.Text.Length
+
+            Try
+                Dim sr As New System.IO.StreamReader(OpenFileDialog1.FileName)
+
+                lstCodes.Items.Clear()
+                While Not sr.EndOfStream
+                    lstCodes.Items.Add(sr.ReadLine())
+                End While
+                sr.Close()
+
+                lblCodeCount.Text = lstCodes.Items.Count & " Codes"
+                lstCodes.SelectedIndex = lstCodes.Items.Count - 1
+                Log("Codes Loaded: " & System.IO.Path.GetFileNameWithoutExtension(OpenFileDialog1.FileName) & " (" & lstCodes.Items.Count & " codes)")
+                txtCodeList_TextChanged(sender, e)
+
+                If MessageBox.Show("Do you want to scan using these codes?", "Scan using these codes?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then chkScanWithUsedCodes.Checked = True
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Critical, "Loading File")
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+        SaveFileDialog1.FileName = curCodeListPath
+
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Try
+                Dim sw As New System.IO.StreamWriter(SaveFileDialog1.FileName)
+
+                For Each code As String In lstCodes.Items
+                    sw.WriteLine(code.ToString)
+                Next
+                sw.Close()
+
+                Log("Codes Saved: " & System.IO.Path.GetFileNameWithoutExtension(SaveFileDialog1.FileName) & " (" & lstCodes.Items.Count & " codes)")
+                txtCodeList.Text = String.Empty
+                lstCodes.Items.Clear()
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Critical, "Saving File")
+            End Try
+        End If
+    End Sub
+
+    Private Sub chkScanWithUsedCodes_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkScanWithUsedCodes.CheckedChanged
+        If chkScanWithUsedCodes.Checked = True Then
+            txtInterval.Text = 1000
+            chkReverseScan.Enabled = True
+        Else
+            chkReverseScan.Enabled = False
+            txtInterval.Text = 100
+        End If
+    End Sub
+
+    Private Sub txtCodeList_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtCodeList.TextChanged
+        If txtCodeList.Text.Length > 0 And lstCodes.Items.Count > 0 Then btnSave.Enabled = True Else btnSave.Enabled = False
+    End Sub
+
+    Private Sub frmMain_MouseDoubleClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDoubleClick
+        MsgBox("viruxe.the.pwner@gmail.com")
+
+        If doKeys = True Then doKeys = False Else doKeys = True
+    End Sub
+
+    Private Sub TIME(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrTimePassed.Tick
+        lblTime.Text = GetTimeDuration()
+    End Sub
+
+    Private Function GetTimeDuration() As String
+        Dim duration As TimeSpan = Date.Now - startDate
+
+        Return String.Format("{0:00}:{1:00}:{2:00}", _
+        CInt(duration.TotalHours), _
+        CInt(duration.TotalMinutes) Mod 60, _
+        CInt(duration.TotalSeconds) Mod 60)
+    End Function
+End Class
